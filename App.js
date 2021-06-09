a0c45597200b2f612730afef3202469a13f5b052
Edgar <cardenasedgar123@gmail.com>
2021-06-09 12:21:23 -0400

    Set up preliminary app layout.

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StatusBar,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.androidSafeArea}>
+        <View style={styles.search}>
+          <Text style={{ backgroundColor: "yellow" }}>Search</Text>
+        </View>
+        <View style={styles.list}>
+          <Text style={{ backgroundColor: "yellow" }}>List</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  androidSafeArea: {
+    // Fills space of parent container
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // Set top padding differently when on android devices. Extracts status bar height from react-native StatusBar so that it can be dynamic
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+  search: {
+    // Since no flex was applied, the container will grow dynamically as content is put inside of it
+    padding: 16,
+    backgroundColor: "green",
+  },
+  list: {
+    // Fills available space within parent containter
+    flex: 1,
+    padding: 16,
+    backgroundColor: "blue",
   },
 });
